54623d7abe5dd277e5d91efb84cb1f681358c987
Mudge13 <mihir0711shetty@gmail.com>
2023-07-29 01:44:34 +0530

    adding new files

---
+++ JPEGtoPNGconverter.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from PIL import Image
+
+# grab first and second args
+starting_loc = sys.argv[1]
+final_loc = sys.argv[2]
+
+print(starting_loc)
+
+# check if new exists if not create new
+if not os.path.exists(final_loc):
+    os.mkdir(final_loc)
+
+# lopp through pokedex and convert files and place it in new
+for root, dirs, all_files in os.walk(starting_loc):
+    for files in all_files:
+        path = f"{starting_loc}/{files}"
+        img = Image.open(path)
+        files = f"changed_{files}"
+        new_path = files.replace("jpg", 'png')
+        new_dir_path = f'{final_loc}/{new_path}'
+        img.save(new_dir_path)
