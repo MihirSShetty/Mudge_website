a0b1a05b1f5879e848280f0ac5a7458d5406595c
Mudge13 <mihir0711shetty@gmail.com>
2023-07-29 01:59:09 +0530

    wrong speeling of loop

--- JPEGtoPNGconverter.py
+++ JPEGtoPNGconverter.py
@@ -12,7 +12,7 @@ print(starting_loc)
 if not os.path.exists(final_loc):
     os.mkdir(final_loc)
 
-# lopp through pokedex and convert files and place it in new
+# loop through pokedex and convert files and place it in new
 for root, dirs, all_files in os.walk(starting_loc):
     for files in all_files:
         path = f"{starting_loc}/{files}"
@@ -21,3 +21,4 @@ for root, dirs, all_files in os.walk(starting_loc):
         new_path = files.replace("jpg", 'png')
         new_dir_path = f'{final_loc}/{new_path}'
         img.save(new_dir_path)
+
